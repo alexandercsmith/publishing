ce8e8faee2dc70a8bb55730a7acecba3a77cebe2
Alexander Smith <acsmith.dev@gmail.com>
2019-02-10 22:41:02 -0500

    update publishing

--- publishing.rb
+++ publishing.rb
@@ -1,6 +1,13 @@
 module Publishing
   extend ActiveSupport::Concern
   included do
+
+    # Set Initial Values
+    before_create do |obj|
+      obj.published = false if obj.has_attribute?(:published)
+      obj.published_at = nil if obj.has_attribute?(:published_at)
+    end
+
     # Model.is_published
     scope :is_published,   -> { where(published: 1) }
     # Model.is_unpublished
@@ -11,28 +18,32 @@ module Publishing
     # Model.all.published_desc
     scope :published_desc, -> { order(published_at: :desc) }
 
+    # @model.publish
+    # Skips Validations
     def publish
+      migration_warning unless check_attr
       update_attribute(:published, 1)
       update_attribute(:published_at, Time.now) if published_at.nil?
     end
 
+    # @model.unpublish
+    # Skips Validations
     def unpublish
+      migration_warning unless check_attr
       update_attribute(:published, 0)
     end
 
     # @model.publish_toggle
     def publish_toggle
-      if published
-        unpublish
-      else
-        publish
-      end
+      migration_warning unless check_attr
+      published ?  unpublish : publish
     end
 
     # @model.publish_check
     # => true: 'published'
     # => false: 'unpublished'
     def publish_check
+      migration_warning unless check_attr
       published ? 'published' : 'unpublished'
     end
 
@@ -40,23 +51,37 @@ module Publishing
     # => true: 'unpublish'
     # => false: 'publish'
     def publish_link
+      migration_warning unless check_attr
       published ? 'unpublish' : 'publish'
     end
 
     # @model.published_date
-    # => Month 00, 0000
-    # => i.e. January 1, 2000
+    # => 00/00/0000 | month/day/year
     def publish_date
-      published ? published_at.strftime('%B %e, %Y') : 'DRAFT'
+      migration_warning unless check_attr
+      published ? published_at.strftime('%D') : 'draft'
     end
 
     # @model.published_time
+    # 12-hour Time
     # => 00:00 AM | PM
-    # => i.e. 4:00 AM
     def publish_time
-      published ? published_at.strftime('%I:%M %p') : 'DRAFT'
+      migration_warning unless check_attr
+      published ? published_at.strftime('%I:%M %p') : 'draft'
     end
-  end
-  class_methods do
+
+    private
+
+    # Verify Object has attributes: :published, :published_at
+    def check_attr
+      has_attribute?(:published) && has_attribute?(:published_at)
+    end
+
+    # Migration Warning
+    def migration_warning
+      logger.warn "[Simple Publish] - WARNING - #{self.class.name.capitalize} Migrations Pending"
+      logger.warn "[Simple Publish] - WARNING - #{self.class.name.capitalize} missing column :published" unless self.has_attribute?(:published)
+      logger.warn "[Simple Publish] - WARNING - #{self.class.name.capitalize} missing column :published_at" unless self.has_attribute?(:published_at)
+    end
   end
 end
